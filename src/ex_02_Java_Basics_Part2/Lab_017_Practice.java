037e785c22ec2f543e665cd42ffd20cd8aed8649
Meghna <meghnabansal167@example.com>
2025-05-12 17:45:01 +0530

    12-05-25

---
+++ src/ex_02_Java_Basics_Part2/Lab_017_Practice.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab_017_Practice {
+    public static void main(String[] args) {
+        String name = "Shambhavi";
+        int Math = 85;
+        int Science = 90;
+        int English = 88;
+        int Total = Math+Science+English;
+        System.out.printf("Report card for %s:%n",name);
+        System.out.printf("%d%n",Math);
+        System.out.printf("%d%n",Science);
+        System.out.printf("%d%n",English);
+        System.out.printf("Total:%d",Total );
+
+    }
+}
