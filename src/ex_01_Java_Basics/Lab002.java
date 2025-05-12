f9314231bc24c87a576891893db19f6c3f884e20
Meghna <meghnabansal167@example.com>
2025-05-12 07:40:26 +0530

    12-05-25

---
+++ src/ex_01_Java_Basics/Lab002.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("this is a statement");
+
+        // example of statement
+    }
+}
